db5d72de29e8fa1c9980e873b06d23baaa0aa78c
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-04-05 12:30:48 +0300

    creating of function that filter lines

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def filter_lines(input_file, keyword):
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
 
+    filtered_lines = [line for line in lines if keyword in line]
+
+    with open("filtered.txt", 'w') as f:
+        f.writelines(filtered_lines)
+
+
+if __name__ == "__main__":
+    filter_lines("input.txt", "example")
