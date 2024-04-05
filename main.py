0728f8b8b65e273806cadd8280399524aec28fd6
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-04-05 13:14:05 +0300

    a few final changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def filter_lines(input_file, keyword):
+def filter_lines(input_file, keyword, output_fine):
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
     filtered_lines = [line for line in lines if keyword in line]
 
-    with open("filtered.txt", 'w') as f:
+    with open(output_fine, 'w') as f:
         f.writelines(filtered_lines)
 
 
 if __name__ == "__main__":
-    filter_lines("input.txt", "example")
+    filter_lines("input.txt", "example", "filtered.txt")
