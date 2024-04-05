96955c7129e1b9112203cdfb2d5dac6cc959b5d7
VolodymyrSorochynsky <sorochinskiyuv@gmail.com>
2024-04-05 13:29:39 +0300

    function documentation

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 def filter_lines(input_file, keyword, output_fine):
+    """
+        Function takes the path to the input file, a keyword, and the path to the output file.
+        Reads the contents of the input file and writes into the output file all lines that contain the specified
+        keyword.
+
+        Parameters:
+        input_file (str): The path to the input file with the extension ".txt".
+        keyword (str): The keyword to search for in the lines of the input file.
+        output_file (str): The path to the output file with the extension ".txt", where the filtered lines will be
+        written.
+
+        Returns:
+        None
+
+        Raises:
+        FileNotFoundError: If the input file does not exist.
+
+        Example:
+        >>> filter_lines("input.txt", "example", "filtered.txt")
+        """
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
